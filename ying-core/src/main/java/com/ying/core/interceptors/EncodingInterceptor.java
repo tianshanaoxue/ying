b1bbee6e9e63d9e446f6bfe868595aa0cf94ce17
liuweies <574613441@qq.com>
2016-03-07 23:07:07 +0800

    修改 ying-utils ying-core

---
+++ ying-core/src/main/java/com/ying/core/interceptors/EncodingInterceptor.java
@@ -0,0 +1,38 @@
+package com.ying.core.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 字符集拦截器
+ * 
+ * @author  张代浩
+ * 
+ */
+public class EncodingInterceptor implements HandlerInterceptor {
+
+	/**
+	 * 在controller后拦截
+	 */
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception) throws Exception {
+
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView modelAndView) throws Exception {
+
+	}
+
+	/**
+	 * 在controller前拦截
+	 */
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+		System.out.println("encoding url="+request.getRequestURL());
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		return true;
+	}
+
+}
